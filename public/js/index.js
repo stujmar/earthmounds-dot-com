3f63418d73abc0faa108c628142c434f1a10acda
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-08-25 22:48:11 -0500

    [sm] cleaned up unsused variables in the index.js to make room for some gsap code

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,7 @@
 console.log("you are on the index.html!");
 window.addEventListener("load", function() {
    let carousel = document.getElementById("banner"); 
-   let thumbtimer, timerIndex = 0, dir="./assets/product-images/";
+   let timerIndex = 0, dir="./assets/product-images/";
    let images = {   
        alt: ["product 01", "product 02", "product 03"],
        pics: [ 
@@ -9,9 +9,7 @@ window.addEventListener("load", function() {
            "product-02.jpg", 
            "product-03.jpg"
         ]};
-    let div = document.createElement("div");
     let img = document.createElement("img");
-    let span = document.createElement("span");
     img.src = dir + images.pics[0];
     img.width = 800;
     img.alt = images.alt[0];
