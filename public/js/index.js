2bc899b3142ce145e6d870f129df3e88068fe0b3
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-08-25 20:15:02 -0500

    [sm] got image carousel to work

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,32 @@
 console.log("you are on the index.html!");
+window.addEventListener("load", function() {
+   let carousel = document.getElementById("banner"); 
+   let thumbtimer, timerIndex = 0, dir="./assets/product-images/";
+   let images = {   
+       alt: ["product 01", "product 02", "product 03"],
+       pics: [ 
+           "product-01.jpg", 
+           "product-02.jpg", 
+           "product-03.jpg"
+        ]};
+    let div = document.createElement("div");
+    let img = document.createElement("img");
+    let span = document.createElement("span");
+    img.src = dir + images.pics[0];
+    img.width = 800;
+    img.alt = images.alt[0];
+    carousel.appendChild(img);
+    timer(timerIndex, dir, images, img);
+});
+
+function timer(timerIndex, dir, images, img) {
+    setInterval(function() {
+        timerIndex++;
+        if (timerIndex > 2) {
+            timerIndex = 0;
+        }
+        img.src = dir + images.pics[timerIndex];
+        img.alt = images.alt[timerIndex];
+    }
+    , 4000);
+}
