d19814b36803ba212afdb1eaa6703b0bff16a2a7
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-08-25 23:24:18 -0500

    [sm] added some pretty sick bounce on the scroll down with gsap, very exited to see what is in store for the future

--- public/js/index.js
+++ public/js/index.js
@@ -28,3 +28,8 @@ function timer(timerIndex, dir, images, img) {
     }
     , 4000);
 }
+
+var tl = gsap.timeline({repeat: 30, repeatDelay: 1});
+tl.to("#conrad", {duration:2, x:300, ease: "bounce"});
+tl.to("#conrad", {duration:2, x:0, ease: "bounce"});
+// gsap.to("#conrad", {duration:2, x:0, ease: "bounce"});
