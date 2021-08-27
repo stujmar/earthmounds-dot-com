4cb49703a66cfa34a4b427550c6162d3f49d8bf0
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-08-26 23:17:12 -0500

    [sm] brought both waves animations a lot closer to done

--- public/js/index.js
+++ public/js/index.js
@@ -29,12 +29,15 @@ function timer(timerIndex, dir, images, img) {
     , 4000);
 }
 
-var tl = gsap.timeline({repeat: 30, repeatDelay: 1});
+var tl = gsap.timeline({repeat: 130, repeatDelay: 0});
 tl.to("#conrad", {duration:2, x:300, ease: "bounce"});
 tl.to("#conrad", {duration:2, x:-500, ease: "bounce"});
 // gsap.to("#conrad", {duration:2, x:0, ease: "bounce"});
 
-var waveOne = gsap.timeline({repeat: 30, repeatDelay: 1});
-waveOne.to("#wave-01", { duration: 2.5, ease: "power1.inOut", x: -250 });
-waveOne.to("#wave-01", { duration: 2.5, ease: "power1.inOut", x: 250 });
+var waveOne = gsap.timeline({repeat: 130, repeatDelay: 0});
+waveOne.to("#wave-01", { duration: 2.5, ease: "power1.inOut", x: 150 });
+waveOne.to("#wave-01", { duration: 2.5, ease: "power1.inOut", x: 0 });
 
+let waveTwo = gsap.timeline({repeat: 30, repeatDelay: 1});
+waveTwo.to("#wave-02", { duration: 2.5, ease: "power1.inOut", x: -150 });
+waveTwo.to("#wave-02", { duration: 2.5, ease: "power1.inOut", x: 0 });
