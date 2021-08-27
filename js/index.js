81f3ff91fe7ab25c778f911177a3ce195f043182
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-08-26 20:20:25 -0500

    [sm] updating changes from main branch to launch on github pages

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 console.log("you are on the index.html!");
 window.addEventListener("load", function() {
    let carousel = document.getElementById("banner"); 
-   let thumbtimer, timerIndex = 0, dir="./assets/product-images/";
+   let timerIndex = 0, dir="./assets/product-images/";
    let images = {   
        alt: ["product 01", "product 02", "product 03"],
        pics: [ 
@@ -9,9 +9,7 @@ window.addEventListener("load", function() {
            "product-02.jpg", 
            "product-03.jpg"
         ]};
-    let div = document.createElement("div");
     let img = document.createElement("img");
-    let span = document.createElement("span");
     img.src = dir + images.pics[0];
     img.width = 800;
     img.alt = images.alt[0];
@@ -30,3 +28,8 @@ function timer(timerIndex, dir, images, img) {
     }
     , 4000);
 }
+
+var tl = gsap.timeline({repeat: 30, repeatDelay: 1});
+tl.to("#conrad", {duration:2, x:300, ease: "bounce"});
+tl.to("#conrad", {duration:2, x:0, ease: "bounce"});
+// gsap.to("#conrad", {duration:2, x:0, ease: "bounce"});
