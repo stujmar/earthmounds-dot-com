bea39059fd56e99eb43e78e1c31e77f9d1c9cd7a
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-08-28 16:34:18 -0500

    [sm] re-upping to pages the new animations to test on mobile

--- js/index.js
+++ js/index.js
@@ -29,7 +29,29 @@ function timer(timerIndex, dir, images, img) {
     , 4000);
 }
 
-var tl = gsap.timeline({repeat: 30, repeatDelay: 1});
+var tl = gsap.timeline({repeat: 130, repeatDelay: 0});
 tl.to("#conrad", {duration:2, x:300, ease: "bounce"});
 tl.to("#conrad", {duration:2, x:0, ease: "bounce"});
 // gsap.to("#conrad", {duration:2, x:0, ease: "bounce"});
+
+var waveOne = gsap.timeline({repeat: 130, repeatDelay: 0});
+waveOne.to("#wave-01", { duration: 2.5, ease: "power1.inOut", x: 150 });
+waveOne.to("#wave-01", { duration: 2.5, ease: "power1.inOut", x: 0 });
+
+let waveOneB = gsap.timeline({repeat: 130, repeatDelay: 0});
+waveOneB.to("#wave-01b", { duration: 2.5, ease: "power1.inOut", x: 150 });
+waveOneB.to("#wave-01b", { duration: 2.5, ease: "power1.inOut", x: 0 });
+
+let waveTwo = gsap.timeline({repeat: 30, repeatDelay: 1});
+waveTwo.to("#wave-02", { duration: 2.5, ease: "power1.inOut", x: -150 });
+waveTwo.to("#wave-02", { duration: 2.5, ease: "power1.inOut", x: 0 });
+
+let waveTwoB = gsap.timeline({repeat: 30, repeatDelay: 1});
+waveTwoB.to("#wave-02b", { duration: 2.5, ease: "power1.inOut", x: -150 });
+waveTwoB.to("#wave-02b", { duration: 2.5, ease: "power1.inOut", x: 0 });
+
+let cloudPathOne = gsap.timeline({repeat: 130, repeatDelay: 0});
+cloudPathOne.to("#cloud-01", {duration: 40, x: screen.width + 250, ease: "power1.inOut"});
+
+let screenWidth = "Total Width: " + screen.width;
+console.log(screenWidth);
