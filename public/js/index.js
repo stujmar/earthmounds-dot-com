77d66ca3afb3cc4b97e83e7a43d702210d435da9
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-08-27 22:23:22 -0500

    [sm] leaps and bounds getting the hills and water spaced ont he bottom

--- public/js/index.js
+++ public/js/index.js
@@ -38,6 +38,10 @@ var waveOne = gsap.timeline({repeat: 130, repeatDelay: 0});
 waveOne.to("#wave-01", { duration: 2.5, ease: "power1.inOut", x: 150 });
 waveOne.to("#wave-01", { duration: 2.5, ease: "power1.inOut", x: 0 });
 
+let waveOneB = gsap.timeline({repeat: 130, repeatDelay: 0});
+waveOneB.to("#wave-01b", { duration: 2.5, ease: "power1.inOut", x: 150 });
+waveOneB.to("#wave-01b", { duration: 2.5, ease: "power1.inOut", x: 0 });
+
 let waveTwo = gsap.timeline({repeat: 30, repeatDelay: 1});
 waveTwo.to("#wave-02", { duration: 2.5, ease: "power1.inOut", x: -150 });
 waveTwo.to("#wave-02", { duration: 2.5, ease: "power1.inOut", x: 0 });
