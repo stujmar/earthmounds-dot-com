8fb241e36b6b170f1f7a827f63174c45d83af40f
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-08-27 21:59:20 -0500

    [sm] got extended wave to touch

--- public/js/index.js
+++ public/js/index.js
@@ -42,5 +42,9 @@ let waveTwo = gsap.timeline({repeat: 30, repeatDelay: 1});
 waveTwo.to("#wave-02", { duration: 2.5, ease: "power1.inOut", x: -150 });
 waveTwo.to("#wave-02", { duration: 2.5, ease: "power1.inOut", x: 0 });
 
+let waveTwoB = gsap.timeline({repeat: 30, repeatDelay: 1});
+waveTwoB.to("#wave-02b", { duration: 2.5, ease: "power1.inOut", x: -150 });
+waveTwoB.to("#wave-02b", { duration: 2.5, ease: "power1.inOut", x: 0 });
+
 let screenWidth = "Total Width: " + screen.width;
 console.log(screenWidth);
